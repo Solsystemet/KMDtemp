5103346ec9ac143709d48ee60325f6d46dae7a8d
Mikkel Burmølle <144026256+Mikkelburmolle@users.noreply.github.com>
2024-10-10 10:54:56 +0200

    button typescript

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -3,14 +3,23 @@ import React from "react";
 
 type ButtonProps = {
    children: React.ReactNode;
+   color: string;
+   backgroundColor?: string;
+   fontSize: string;
+   border?: string;
    onClick: () => void;
-   style?: React.CSSProperties;
 };
 
-export function Button({ children, onClick, style }: ButtonProps) {
+export function Button(props: ButtonProps) {
    return (
-      <button style={style} className={styles.filledButton} onClick={onClick}>
-         {children}
+      <button style={{
+         color: props.color,
+         backgroundColor:props.backgroundColor,
+         fontSize: props.fontSize,
+         border: props.border,
+      }
+         } className={styles.Button} onClick={props.onClick}>
+         {props.children}
       </button>
    );
 }
