fbbba2f1d558dd52a1e82d8a024f79a45d36a28a
Jacob Mølholt Poulsen <43791907+lindejacob@users.noreply.github.com>
2024-10-14 17:34:20 +0200

    Made progressbar, but needs some context to finish it

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../../components/buttons/Buttons";
 import { Navbar } from "../../components/navbar/Navbar";
+import { ProgressBar } from "../../components/progressBar/ProgressBar";
 
 export function Homepage() {
    return (
@@ -15,10 +16,15 @@ export function Homepage() {
 }
 
 export function Test() {
+   const handleClick1 = () => console.log("Clicked 1");
+   const handleClick2 = () => console.log("Clicked 2");
+   const handleClick3 = () => console.log("Clicked 3");
+
    return (
-      <Navbar>
-         <span>Listings</span>
-         <span>Create Listing</span>
-      </Navbar>
+      <ProgressBar onClickHandlers={[handleClick1, handleClick2, handleClick3]}>
+         <span>Lorem ibsum</span>
+         <span>Lorem ibsum</span>
+         <span>Lorem ibsum</span>
+      </ProgressBar>
    );
 }
