4aea6943987ab653080571bcc4ae0937bdf5975c
Mikkel Burmølle <144026256+Mikkelburmolle@users.noreply.github.com>
2024-10-14 12:35:07 +0200

    Homepage fix

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../../components/buttons/Buttons";
+
+
+export function Homepage(){
+    return(
+        <Button children={undefined} color={""} fontSize={""} onClick={function (): void {
+            throw new Error("Function not implemented.");
+        } }/>
+    )
+}
