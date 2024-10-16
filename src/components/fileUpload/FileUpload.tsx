102110effc00189318ae287e954e05b7be6ad5c5
Jacob Mølholt Poulsen <43791907+lindejacob@users.noreply.github.com>
2024-10-16 10:34:42 +0200

    Created FileDragAndDrop + merged it with fileUpload

--- src/components/fileUpload/FileUpload.tsx
+++ src/components/fileUpload/FileUpload.tsx
@@ -1,35 +1,20 @@
-import React, { useRef, useState } from "react";
+import React, { useCallback, useState } from "react";
+import styles from "./FileUpload.module.css";
+import { useDropzone } from "react-dropzone";
 import axios from "axios";
-import styles from "./fileUpload.module.css";
-import { Button } from "../buttons/Buttons";
 import { FileText } from "lucide-react";
 
 export function FileUpload() {
-   const inputRef = useRef<HTMLInputElement | null>(null);
+   const [uploadStatus, setUploadStatus] = useState("select");
    const [selectedFile, setSelectedFile] = useState<File | null>(null);
    const [progress, setProgress] = useState(0);
-   const [uploadStatus, setUploadStatus] = useState("select");
-
-   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.files && event.target.files[0]) {
-         setSelectedFile(event.target.files[0]);
-      }
-   };
-
-   const onChooseFile = () => {
-      inputRef.current?.click();
-   };
 
    const clearFileInput = () => {
-      if (inputRef.current) {
-         inputRef.current.value = "";
-      }
       setSelectedFile(null);
       setProgress(0);
-      setUploadStatus("select");
    };
 
-   const handleUpload = async () => {
+   const handleUpload = async (file: File) => {
       if (uploadStatus === "done") {
          clearFileInput();
          return;
@@ -37,9 +22,7 @@ export function FileUpload() {
       try {
          setUploadStatus("Uploading");
          const formData = new FormData();
-         if (selectedFile) {
-            formData.append("file", selectedFile);
-         }
+         formData.append("file", file);
 
          await axios.post("http://localhost:5173/api/upload", formData, {
             onUploadProgress: (progressEvent: any) => {
@@ -56,76 +39,43 @@ export function FileUpload() {
       }
    };
 
+   const doDrop = useCallback(acceptedFiles => {
+      if (acceptedFiles.length > 0) {
+         const file = acceptedFiles[0];
+         setSelectedFile(file);
+         handleUpload(file);
+      }
+   }, []);
+
+   const { getRootProps, getInputProps, isDragActive } = useDropzone({
+      onDrop: doDrop,
+   });
+
    return (
-      <div className={styles.container}>
-         <input
-            ref={inputRef}
-            type="file"
-            onChange={handleFileChange}
-            className={styles.hiddenInput}
-         />
+      <>
+         <div {...getRootProps()} className={styles.dropZone}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+               <p>Drop the file here</p>
+            ) : (
+               <p>Drag and drop the contact here, or click to select files</p>
+            )}
+         </div>
 
-         {!selectedFile ? (
-            <Button
-               color={"White"}
-               fontSize={"1rem"}
-               backgroundColor="var(--secondary-color)"
-               onClick={onChooseFile}
-            >
-               <span>Upload file</span>
-            </Button>
-         ) : (
-            <>
-               <div className={styles.fileInfo}>
-                  <div className={styles.fileDetails}>
-                     <div className={styles.picCircle}>
-                        <FileText size={"70%"} />
-                     </div>
-                     <p className={styles.fileName}>{selectedFile.name}</p>
-                     <div className={styles.progressBg}>
-                        <div
-                           className={styles.progress}
-                           style={{ width: `${progress}%` }}
-                        />
-                     </div>
-                  </div>
-                  {uploadStatus === "select" ? (
-                     <Button
-                        onClick={clearFileInput}
-                        color={"var(--primary-color)"}
-                        backgroundColor={"transparent"}
-                        fontSize={"1rem"}
-                     >
-                        <span>Choose other file</span>
-                     </Button>
-                  ) : (
-                     <div className={styles.checkCircle}>
-                        {uploadStatus === "Uploading" ? (
-                           <span>hourglass_empty</span>
-                        ) : uploadStatus === "done" ? (
-                           <span
-                              className="material-symbols-outlined"
-                              style={{ fontSize: "20px" }}
-                           >
-                              check
-                           </span>
-                        ) : null}
-                     </div>
-                  )}
+         {selectedFile && (
+            <div className={styles.fileDetails}>
+               <div className={styles.picCircle}>
+                  <FileText size={"70%"} />
                </div>
-
-               <Button
-                  onClick={handleUpload}
-                  backgroundColor={"var(--secondary-color)"}
-                  color={"white"}
-                  fontSize={"1rem"}
-               >
-                  {uploadStatus === "select" || uploadStatus === "Uploading"
-                     ? "Submit"
-                     : "Done"}
-               </Button>
-            </>
+               <p className={styles.fileName}>{selectedFile.name}</p>
+               <div className={styles.progressBg}>
+                  <div
+                     className={styles.progress}
+                     style={{ width: `${progress}%` }}
+                  />
+               </div>
+            </div>
          )}
-      </div>
+      </>
    );
 }
--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -3,26 +3,9 @@ import { Button } from "../../components/buttons/Buttons";
 import { Navbar } from "../../components/navbar/Navbar";
 import { ProgressBar } from "../../components/progressBar/ProgressBar";
 import { Searchbar } from "../../components/searchbar/Searchbar";
-
-
-export function Homepage() {
-   return (
-      <Searchbar value={"Thi is just as test to see if it adjusts"} searchHandler={function (): void {
-         throw new Error("Function not implemented.");
-      } } placeholder={"Type any text"}/>
-   )
-}
+import { FileUpload } from "../../components/fileUpload/FileUpload";
+export function Homepage() {}
 
 export function Test() {
-   const handleClick1 = () => console.log("Clicked 1");
-   const handleClick2 = () => console.log("Clicked 2");
-   const handleClick3 = () => console.log("Clicked 3");
-
-   return (
-      <ProgressBar onClickHandlers={[handleClick1, handleClick2, handleClick3]}>
-         <span>Lorem ibsum</span>
-         <span>Lorem ibsum</span>
-         <span>Lorem ibsum</span>
-      </ProgressBar>
-   );
+   return <FileUpload />;
 }
