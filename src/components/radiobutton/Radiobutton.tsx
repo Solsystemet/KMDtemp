847ae74483764ff3477f1c52aa72dbf5f615555d
Mikkel Burmølle <144026256+Mikkelburmolle@users.noreply.github.com>
2024-10-22 20:43:03 +0200

    radioButton

--- src/components/radiobutton/Radiobutton.tsx
+++ src/components/radiobutton/Radiobutton.tsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Radiobutton.module.css";
 
 type RadioButtonProps = {
-    label: string, 
-    name: string,
-    value: string,
-    checked: boolean,
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    children: string [];
 };
 
-export function Radiobutton(props: RadioButtonProps) {
-    return(
-        <label className="{styles.RadioButtonLabel">
-        <input
-        type="radio"
-        name={props.name}
-        value={props.value}
-        checked={props.checked}
-        onChange={props.onChange}
-        className={styles.RadioButtonInput}/>
-        {props.label}
-        </label>
+export function RadioButtonSelect(props: RadioButtonProps) {
+    const [selectedIndex, setSelectedIndex] =useState<number |null>(null);
+
+    const handleChange = (index: number) => {
+        setSelectedIndex(index);
+    };
+
+    return (
+        <div className={styles.radioButton}>
+            {props.children.map((option,index) => (
+                <div key={index} className={styles.option}>
+                    <label className={styles.customRadio}>
+                        <input
+                        type="radio"
+                        name="radioGroup"
+                        checked={selectedIndex === index}
+                        onChange={() => handleChange(index)}
+                        />
+                        <span className={styles.radiomark}></span>
+                        {option}
+                    </label>
+                </div>
+            ))}
+        </div>
     )
 }
--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "../../components/navbar/Navbar";
 import { MultipleSelect } from "../../components/multipleSelect/MultipleSelect";
+import { RadioButtonSelect } from "../../components/radiobutton/Radiobutton";
 
 export function Homepage() {
    return (
@@ -18,6 +19,13 @@ export function Homepage() {
             {"Option 3"}
             {"Option 4"}
          </MultipleSelect>
+
+         <RadioButtonSelect>
+            {"Option 1"}
+            {"Option 2"}
+            {"Option 3"}
+         </RadioButtonSelect>
+
       </>
    );
 }
